1333d5aced433983a93109e7decdf6374af6b917
Editxd <152770568+Editxd@users.noreply.github.com>
2023-12-09 14:17:37 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 9327552
 api_hash = "9f62041fb0563f1780f52d07a6e470b5"
-bot_token = "6570587434:AAHdlWmCHTVvNchadFxvYdCb5E73G7IrBKw"
+bot_token = "6836997893:AAE5ZB-q50k3dgNrYdNiKGOJ6qtRQ_CRlO0"
 
